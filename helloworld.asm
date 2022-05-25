cb2458b0e46e902b32dbe32b718b50fa2918e6b0
Stuart Thompson <stuart@stuartthompson.net>
2022-05-25 15:23:07 -0700

    Changes to hard-coded message length.
    Shows how to mov a constant into a register (the type of the constant must be specified correctly)

--- helloworld.asm
+++ helloworld.asm
@@ -4,16 +4,19 @@ global _start
 
 section .text
 _start:
+    ; Write hello to stdout
     mov eax, 0x4                ; syscall write
     mov ebx, 1                  ; file descriptor stdout
-    mov ecx, greeting           ; buf
-    mov edx, greeting_length    ; count
+    mov ecx, greeting           ; message
+    ; [len] vs len because we want value not address
+    mov edx, [len]              ; message length
     int 0x80                    ; syscall write
 
+    ; Exit with return code 0
     mov eax, 0x1                ; syscall exit
     mov ebx, 0x0                ; return value 0
     int 0x80                    ; syscall exit
 
 section .data
     greeting: db "Hello, World!", 0xA
-    greeting_length equ $-greeting
+    len: dd 0xe                 ; dd = dword
